91f97002e40ae9dd749e0872b2da755d20f800de
malandr <quadr988@gmail.com>
2022-09-04 20:14:56 +0300

    divs

--- Main.hs
+++ Main.hs
@@ -1,26 +1,25 @@
 module Main where
 
 import Data.Char
--- import Network.HTTP
-import qualified Data.ByteString.Lazy.Char8 as L8
+import Network.HTTP
+import Network.HTTP.Conduit
+-- import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy as LB
 -- import Network.HTTP.Simple
-import Network.HTTP.Client
-import Network.HTTP.Types.Status (statusCode)
+-- import Network.HTTP.Client
+-- import Network.HTTP.Types.Status (statusCode)
 import Text.HTML.TagSoup
+-- improt Data.Text.StringLike
 import Data.Typeable
 
--- getData :: String -> String
-
+--simpleHttp "https://stackoverflow.com" --LB.putStr
 
 main :: IO ()
 main = do
-   manSettings <- managerTlsConnection defaultManagerSettings 
-   hostAddrr <- tupleToHostAddress ()
-   manager <- newManager (manSettings (hostAddrr, "", 80)) --defaultManagerSettings
-
-   request <- parseRequest "http://en.wikipedia.org/wiki/French_Revolutionary_Wars"
-   response <- httpLbs request manager
-
---    putStrLn $ "The status code was: " ++ 
---         (show $ statusCode $ responseStatus response)
-   putStrLn $ show $ typeOf $ responseBody response
+    req <- simpleHttp "https://en.wikipedia.org/wiki/Main_Page"
+    let allTaggs = parseTags req
+    let mainTag = head (take 1 allTaggs)
+    --let links = [tag | tag <- allTaggs, tag ]
+    -- let links = map f $ sections (~== "<A>")
+    let txt = fromTagText (dropWhile (~/= "<div id=mw-content-text>") mainTag)
+    print txt
